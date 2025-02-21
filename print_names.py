117536bcfe9eaec761a261173fec1ea66ec07a01
iii <156879644+tttalliu@users.noreply.github.com>
2025-02-21 23:44:32 +0500

    Update print_names.py

--- print_names.py
+++ print_names.py
@@ -1,1 +1,12 @@
-print("i")
+import sys
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python names_cli.py name1 name2 ...")
+        return
+
+    for name in sys.argv[1:]:
+        print(name)
+
+if __name__ == "__main__":
+    main()
