338093a4fbb7e77a53c208cd3f0487408904f0f7
iii <156879644+tttalliu@users.noreply.github.com>
2025-02-22 11:10:53 +0500

    Update b.py

--- b.py
+++ b.py
@@ -5,7 +5,8 @@ def greet(name):
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        name = sys.argv[1] 
+    names = sys.argv[1:]
+    for name in names:
         greet(name)
     else:
-        print("Usage: python b.py <name>")
+        print("Write name")
